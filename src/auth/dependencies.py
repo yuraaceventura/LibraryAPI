65261371d60d1a84fb1a5c723317e78dc79682f0
yuraaceventura <yuraaceventura@gmail.com>
2025-05-16 19:20:15 +0300

    Created dependencies for auth module

---
+++ src/auth/dependencies.py
@@ -0,0 +1,70 @@
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt import PyJWTError
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.annotation import Annotated
+
+from src.auth.schemas import TokenData
+from src.auth.utils import verify_password, get_password_hash
+from database import User
+from config.config import settings
+from database.db_helper import db_helper
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+async def get_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
+    username: str,
+):
+    stmt = select(User).where(User.username == username)
+    result: User = await session.scalar(stmt)
+    return result
+
+
+async def authenticate_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
+    username: str,
+    password: str,
+):
+    user: User = await get_user(session, username)
+    if not user:
+        return False
+    print(password, user.hashed_password)
+    print(get_password_hash(password))
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exceptions = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.jwt.public_key, algorithms=[settings.jwt.algorithm]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exceptions
+        token_data = TokenData(
+            username=username, access_token=token, token_type="Bearer"
+        )
+    except PyJWTError:
+        raise credentials_exceptions
+    user = await get_user(
+        session,
+        token_data.username,
+    )
+    if user is None:
+        raise credentials_exceptions
+    return user
