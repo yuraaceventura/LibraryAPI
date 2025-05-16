2ae5d6e9fa65dad84bb57a66a8db935d55d4e7fa
yuraaceventura <yuraaceventura@gmail.com>
2025-05-17 00:05:47 +0300

    Added books router to main app

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from src.auth.views import router as auth_router
+from src.books.views import router as books_router
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(books_router)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=1)
