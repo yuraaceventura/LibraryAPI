4ca000c0a7e593d0948ef70b8ba4c88ecc93444a
yuraaceventura <yuraaceventura@gmail.com>
2025-05-16 20:29:07 +0300

    Updated model

--- database/models/ReaderModel.py
+++ database/models/ReaderModel.py
@@ -3,15 +3,11 @@ from sqlalchemy import CheckConstraint, String
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from database.base import Base
-from database.models.BookModel import BookModel
-
 
 class ReaderModel(Base):
     __tablename__ = 'readers'
-    __table_args__ = (
-        CheckConstraint("in_stock >= 0", name="check_in_stock_constraint"),
-    )
+
     id : Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     email: Mapped[EmailStr] = mapped_column(String, index=True, unique=True)
-    borrowed = relationship("borrowed_books", back_populates="reader_id")
+    borrowed = relationship("BorrowedBooksTable", back_populates="reader")
