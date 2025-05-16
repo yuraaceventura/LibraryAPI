4a4439ba74295d0d5154be7fc3f139c45c71e8c5
yuraaceventura <yuraaceventura@gmail.com>
2025-05-16 19:20:46 +0300

    setup __all__ magic method for import

---
+++ database/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "UserModel",
+    "BorrowedBooksTable",
+    "BookModel",
+    "ReaderModel"
+]
+
+from database.models.UserModel import UserModel
+from database.models.BorrowedBooksTable import BorrowedBooksTable
+from database.models.BookModel import BookModel
+from database.models.ReaderModel import ReaderModel
