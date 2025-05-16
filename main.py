08477b119825df558b2ac18d4174b81d5f996b67
yuraaceventura <yuraaceventura@gmail.com>
2025-05-16 20:03:45 +0300

    added main app and imported auth endpoints

---
+++ main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from fastapi import FastAPI
+from src.auth.views import router as auth_router
+app = FastAPI()
+
+app.include_router(auth_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=1)
