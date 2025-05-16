893cdb2bfebcccc2658192701e15997bca5a18e0
yuraaceventura <yuraaceventura@gmail.com>
2025-05-17 00:05:06 +0300

    Fixed schemas

---
+++ src/books/schemas.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class BookBase(BaseModel):
+    title: str
+    author: str
+    published_at: int | None
+    ISBN: str | None
+    available: int
+
+class BookRead(BookBase):
+    id: int
+
+
+class BookUpdate(BookBase):
+    title: str | None
+    author: str | None
+    published_at: int | None
+    ISBN: str | None
+    available: int | None
