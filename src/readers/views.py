c60a9eebf80f8100bf8e6f49353d1c40aa65f609
yuraaceventura <yuraaceventura@gmail.com>
2025-05-17 20:00:52 +0300

    Minor fixes

--- src/readers/views.py
+++ src/readers/views.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
+from starlette.status import HTTP_409_CONFLICT
 
 from database.db_helper import db_helper
 from src.auth.dependencies import get_current_user
 from src.readers import utils
-from src.readers.schemas import ReaderUpdate, ReaderBase
+from src.readers.schemas import ReaderUpdate, ReaderBase, ReaderFull
 
 router = APIRouter(prefix="/readers", tags=["Readers"])
 
@@ -19,21 +20,30 @@ async def get_readers(session: Annotated[AsyncSession, Depends(db_helper.get_ses
 @router.post("", dependencies=[Depends(get_current_user)])
 async def create_reader(session: Annotated[AsyncSession, Depends(db_helper.get_session)],
                       reader: ReaderBase,
-                      ) -> ReaderBase:
-    return await utils.create_reader(session, reader)
+                      ) -> ReaderFull:
+    reader = await utils.create_reader(session, reader)
+    if reader:
+        return reader
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Reader already exists")
+
 
 @router.get("/{reader_id}", dependencies=[Depends(get_current_user)])
 async def get_reader_by_id(session: Annotated[AsyncSession, Depends(db_helper.get_session)],
-                         reader_id: int,):
-    return await utils.get_reader(session, reader_id)
+                         reader_id: int,) -> ReaderFull:
+    reader = await utils.get_reader(session, reader_id)
+    if reader:
+        return reader
+    else:
+        raise HTTPException(status_code=HTTP_409_CONFLICT, detail="Reader does not exist")
+
 
 @router.delete("/{reader_id}", dependencies=[Depends(get_current_user)])
 async def delete_reader(session: Annotated[AsyncSession, Depends(db_helper.get_session)],
-                        reader_id: int,):
+                        reader_id: int,) -> ReaderFull:
     reader = await utils.delete_reader(session, reader_id)
     if reader:
-        return {"msg": "reader deleted successfully",
-                "reader" : reader}
+        return reader
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Couldn't find this reader")
@@ -42,10 +52,10 @@ async def delete_reader(session: Annotated[AsyncSession, Depends(db_helper.get_s
 @router.patch("/{reader_id}", dependencies=[Depends(get_current_user)])
 async def update_reader(session: Annotated[AsyncSession, Depends(db_helper.get_session)],
                       reader_id: int,
-                      reader: ReaderUpdate) -> ReaderBase:
+                      reader: ReaderUpdate) -> ReaderFull:
     if len(reader.model_dump()) > 0:
         reader_updated = await utils.update_reader(session, reader_id, reader)
-        return ReaderBase.model_validate(reader_updated.__dict__)
+        return reader_updated
 
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                         detail="Something went wrong")
