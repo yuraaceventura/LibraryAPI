bf159931b95057686aaa4a2e81fd41f1bc42431a
yuraaceventura <yuraaceventura@gmail.com>
2025-05-17 15:31:05 +0300

    Finished auth tests

---
+++ tests/test_auth.py
@@ -0,0 +1,16 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_unauthed_login(client: AsyncClient):
+    response = await client.post(
+        "/auth/login",
+        json={"email": "wrong@example.com", "password": "wrong"}
+    )
+    assert response.status_code == 401
+
+@pytest.mark.asyncio
+async def test_authed_login(auth_client: AsyncClient):
+    response = await auth_client.post("/auth/login", json={"email": "user@example.com", "password": "string"})
+    assert response.status_code == 200
