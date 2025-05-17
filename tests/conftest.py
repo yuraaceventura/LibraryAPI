2e6e81cad7346993fe8af1dfb53fdacc8f6182c7
yuraaceventura <yuraaceventura@gmail.com>
2025-05-17 18:37:59 +0300

    Finished test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
+from sqlalchemy import delete, text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from config.config import settings
@@ -9,18 +10,36 @@ from main import app
 
 @pytest_asyncio.fixture(loop_scope="session", autouse=True)
 async def setup_db():
-    engine = create_async_engine(settings.database.get_url())
-    async with engine.begin() as conn:
-        Base.metadata.schema = "test"
-        await conn.run_sync(Base.metadata.create_all)
-    yield
+    engine = create_async_engine(settings.database.get_url_test())
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+@pytest_asyncio.fixture(autouse=True, scope="function")
+async def truncate_tables():
+    engine = create_async_engine(settings.database.get_url_test())
+    async with engine.begin() as conn:
+        for table in reversed(Base.metadata.sorted_tables):
+            await conn.execute(text(f'TRUNCATE "{table.name}" RESTART IDENTITY CASCADE;'))
+    yield
+
+
+@pytest_asyncio.fixture
+async def db_session():
+    engine = create_async_engine(settings.database.get_url_test())
+    session_maker = async_sessionmaker(
+        engine, expire_on_commit=False
+    )
+    async with session_maker() as session:
+        yield session
 
 
 @pytest_asyncio.fixture
 async def client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test", follow_redirects=True) as client:
         yield client
 
 
@@ -38,15 +57,4 @@ async def auth_client(client: AsyncClient):
 
     client.cookies = login_response.cookies
     return client
-
-
-@pytest_asyncio.fixture
-async def session():
-    engine = create_async_engine(settings.database.get_url())
-    async_session = async_sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
-        yield session
-        await session.close()
 
